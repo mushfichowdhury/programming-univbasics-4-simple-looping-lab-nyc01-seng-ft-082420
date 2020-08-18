55004ced7afb549db647153c3d4fdcad485e8691
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-18 07:31:22 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,4 +6,12 @@ def loop_message_five_times(string)
   end
 end
 
-    
+def loop_message_n_times(string, limit)
+  count = 0 
+  while count < limit do
+    puts string
+    count += 1 
+  end
+end
+
+  
